03c08e56e18d212006592f15bc9012c387aad26f
daniel09077 <danielokuguni15@gmail.com>
2026-07-30 12:21:28 -0700

    feat: refactor group and user creation logic; consolidate user groups into associative array

--- resetup.sh
+++ resetup.sh
@@ -28,27 +28,27 @@ sleep 0.5
 #-----
 #-----CREATE GROUP BLOCK
 echo "Creating groups for the organization"
-groups=("developers" "finance" "hr" "management" "shared")
-creategroup(){
-    sudo groupadd "$1"
-}
-for group in "${groups[@]}"
-do 
-    creategroup "${group}"
-done
-#-----
-#-----CREATE GROUP DONE
-sleep 0.5
-#-----
-#-----CREATE USERS BLOCK
-echo "Creating users for the organization"
-users=("alice" "john" "mike" "sarah" "david" "grace" "linda" "ceo" "manager")
+declare -A user_groups
+user_groups["developers"]="alice john mike"
+user_groups["finance"]="sarah david"
+user_groups["hr"]="grace linda"  
+user_groups["management"]="ceo manager"
+
 createuser(){
-    sudo useradd -m -g "$1" -s /bin/bash "$2"
+    sudo useradd -m -g "${1}" -s /bin/bash "${2}"
 }
-for user in "${users[@]}"
-do 
-    createuser "${group}" "$user"
+for usergroup in "${!user_groups[@]}"
+    do 
+        for user in "${user_groups[$usergroup]}"
+        do
+            createuser "${usergroup}" "${user}"
+        done
+    done
+
+shared_users=("alice" "john" "mike" "sarah" "david" "grace" "linda" "ceo" "manager")  
+for s in "${shared_users[@]}"
+do
+    sudo usermod -aG shared "$s" 
 done
 #-----
 #-----CREATE USERS DONE
