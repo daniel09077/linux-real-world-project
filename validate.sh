0eb23c5e43f3c5a1aa0df0b3d4a815d9deadd241
daniel09077 <danielokuguni15@gmail.com>
2026-07-31 10:11:46 -0700

    fix: correct base path definition and clean up user group validation logic

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #--- Check path is created successfully or not
-base_path="/linux-sim2-env/company"
+base_path="linux-sim2-env/company"
 echo "Checking base path created successfully or not"
 if [ -d "$base_path" ]; then
     echo "[PASS] Base path created successfully."
@@ -18,6 +18,7 @@ do
 else
     echo "[FAIL]Failed to create ${depart}."
 fi
+done
 #----
 
 #---- check users and groups created or not
@@ -27,26 +28,12 @@ user_groups["developers"]="alice john mike"
 user_groups["finance"]="sarah david"
 user_groups["hr"]="grace linda"  
 user_groups["management"]="ceo manager"
-
-
-for usergroup in "${!user_groups[@]}"
-    do 
-        for user in "${user_groups[$usergroup]}"
-            do
-                if id "$user" &>/dev/null; then
-                    echo "[PASS] ${user exists"
-                else
-                    echo "[FAIL] $user does not exist"
-                fi
-        done
-done
-
-shared_users=("alice" "john" "mike" "sarah" "david" "grace" "linda" "ceo" "manager")  
-for s in "${shared_users[@]}"
-do
-    if groups "$s" | grep -q "\bshared\b"; then
-        echo "[PASS] $s is a member of the shared group"
+secgroup="shared"
+for users in "${!user_groups[@]}"
+do 
+    if id -nG "$users" | grep -qw "$secgroup"; then
+        echo "[PASS] $user is a member of $secgroup group."
     else
-        echo "[FAIL] $s is not a member of the shared group"
+        echo "[FAIL] $user is not a member of $secgroup group."
     fi
 done
