861bf91d8b6f0a343d58f2e179f913353b12164c
daniel09077 <danielokuguni15@gmail.com>
2026-07-31 10:32:42 -0700

    fix: correct base path definition to include home directory and add department files validation

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #--- Check path is created successfully or not
-base_path="linux-sim2-env/company"
+base_path="~/linux-sim2-env/company"
 echo "Checking base path created successfully or not"
 if [ -d "$base_path" ]; then
     echo "[PASS] Base path created successfully."
@@ -37,3 +37,24 @@ do
         echo "[FAIL] $user is not a member of $secgroup group."
     fi
 done
+
+declare -A department_files 
+department_files["engineering"]="backend.py frontend.js README.md"
+department_files["finance"]="salary.xlsx budget.xlsx tax.pdf announcement.txt company_calender.pdf"
+department_files["hr"]="employees.csv leave_policy.pdf"    
+department_files["management"]="strategy.pdf meeting_notes.txt"
+department_files["shared"]="shared_doc.txt annoucement.txt company_calender.pdf"
+
+
+for d in "${!department_files[@]}"
+do 
+   for file in ${department_files[$d]}
+   do
+    if [ -f "$base_path/$d/$file" ]; then 
+        echo "[PASS] $file is created successfully in $d."
+    else
+        echo "[FAIL] $file is not created in $d."
+    fi
+done
+done
+
